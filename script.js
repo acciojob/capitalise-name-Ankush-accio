6545e6bf6e0d4a0aabed0706029baa4d3a5f4ae8
Ankush-accio <ankushrohilla2304@gmail.com>
2024-11-15 14:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let input = document.getElementById("fname")
+
+input.addEventListener("blur" , () => {
+	let temp = input.value.toUpperCase()
+	input.value = temp
+})
